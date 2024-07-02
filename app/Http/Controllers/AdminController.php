339b8ebe047e017767d8ef63a3360a55f71e2b86
biswa <bnsahoo@gmail.com>
2024-07-02 12:22:33 +0530

    forget-password

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use App\Mail\forgerpasswordmail;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Str;
 
 class AdminController extends Controller
 {
@@ -13,4 +17,26 @@ class AdminController extends Controller
     public function list(){
         return view('admin.admin.list');
     }
+
+    public function forget_password(){
+        return view('auth.forget_password');
+    }
+
+    public function postForgotPassword(Request $request) {
+        // Validate the email input
+        $request->validate(['email' => 'required|email']);
+
+        $user = User::getEmailSingle($request->email);
+
+        if ($user !== null) {
+            $user->remember_token = Str::random(30);
+            $user->save();
+            Mail::to($user->email)->send(new forgerpasswordmail($user));
+
+            return redirect()->back()->with('success', 'Please check your email to reset your password');
+        } else {
+            return redirect()->back()->with('error', 'Email not found');
+        }
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\StudentController;
 use App\Http\Controllers\TeacherController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 Route::get('/', function () {
     return view('auth.login');
@@ -25,10 +27,14 @@ require __DIR__.'/auth.php';
 
 Route::get('admin/dashboard', [AdminController::class , 'login'])->middleware(['auth' , 'admin']);
 
-Route::get('admin/admin/list', [AdminController::class , 'list']);
+Route::get('admin/admin/list', [AdminController::class , 'list'])->middleware(['auth' , 'admin']);
 
 Route::get('student/dashboard' , [StudentController::class , 'login'])->middleware(['auth' , 'student']);
 
 Route::get('teacher/dashboard' , [TeacherController::class , 'login'])->middleware(['auth', 'teacher']);
 
 Route::get('parent/dashboard' , [ParentController::class , 'login'])->middleware(['auth' , 'parent']);
+
+Route::get('/forget-password' , [AdminController::class , 'forget_password']);
+
+Route::post('forgot-password' , [AdminController::class , 'postForgotPassword']);
